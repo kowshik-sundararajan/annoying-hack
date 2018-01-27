dfcdb2ab4eece802bf837c1e4958b0548fccc40d
Kowshik Sundararajan <kowshik@versafleet.co>
2018-01-28 05:28:30 +0800

    Send translation as response

--- index.js
+++ index.js
@@ -12,57 +12,75 @@ const port = 3000;
 
 let languages = '';
 let target = '';
+let translatedText = '';
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
 function detectAndTranslateLanguages() {
-  translate
-    .detect(text)
-    .then((results) => {
-      let detections = results[0];
-      detections = Array.isArray(detections) ? detections : [detections];
+  return new Promise((resolve, reject) => {
+    translate
+      .detect(text)
+      .then((results) => {
+        let detections = results[0];
+        detections = Array.isArray(detections) ? detections : [detections];
 
-      console.log('Detections:');
-      detections.forEach((detection) => {
-        console.log(`${detection.input} => ${detection.language}`);
-        translate
-          .translate(text, target)
-          .then((response) => {
-            const translation = response[0];
+        console.log('Detections:');
+        detections.forEach((detection) => {
+          console.log(`${detection.input} => ${detection.language}`);
+          translate
+            .translate(text, target)
+            .then((response) => {
+              const translation = response[0];
+              console.log(`Text: ${text}`);
+              console.log(`Translation: ${translation}`);
 
-            console.log(`Text: ${text}`);
-            console.log(`Translation: ${translation}`);
-          })
-          .catch((err) => {
-            console.error('ERROR:', err);
-          });
+              resolve(translation);
+            })
+            .catch((err) => {
+              console.error('ERROR:', err);
+              reject(err);
+            });
+        });
+      })
+      .catch((err) => {
+        console.error('ERROR:', err);
+        reject(err);
       });
-    })
-    .catch((err) => {
-      console.error('ERROR:', err);
-    });
+  });
 }
 
 // Lists available translation language with their names in English (the default).
 function listLanguages() {
-  translate
-    .getLanguages()
-    .then((results) => {
-      [languages] = results;
-      const randomNumber = getRandomInt(languages.length);
-      target = languages[randomNumber].code;
-      detectAndTranslateLanguages();
-    })
-    .catch((err) => {
-      console.error('ERROR:', err);
-    });
+  return new Promise((resolve, reject) => {
+    translate
+      .getLanguages()
+      .then((results) => {
+        [languages] = results;
+        const randomNumber = getRandomInt(languages.length);
+        target = languages[randomNumber].code;
+        detectAndTranslateLanguages().then((response) => {
+          translatedText = response;
+          resolve();
+        });
+      })
+      .catch((err) => {
+        console.error('ERROR:', err);
+        reject();
+      });
+  });
 }
 
 app.get('/', (req, res) => {
-  listLanguages();
-  res.send('Hello world!');
+  listLanguages().then(() => {
+    console.log('this is the translated text', translatedText);
+    res.send(translatedText);
+  })
+    .catch((err) => {
+      console.error(err);
+      res.send('There was an error!');
+    });
 });
 
 app.listen(port, () => console.log(`app started on port ${port}`));
