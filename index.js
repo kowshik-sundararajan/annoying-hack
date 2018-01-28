058448a5260cea7095bcfca287e4a97284302b9b
Kowshik Sundararajan <kowshik@versafleet.co>
2018-01-28 11:47:23 +0800

    Adjust process env port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const { projectId } = require('./config.js');
 
 const app = express();
-const port = process.env.NODE_ENV || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
